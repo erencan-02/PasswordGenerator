c103444f1193254d7d3e810b99e9df9b48e54eab
Eren <erencan11235@outlook.de>
2020-06-23 19:35:06 +0200

    First commit

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,13 +3,13 @@ import string
 
 class PasswordGenerator:
     def __init__(self):
-        self.container = ["", string.ascii_lowercase, string.digits, string.punctuation]
+        self.container = [string.ascii_uppercase, string.ascii_lowercase, string.digits, string.punctuation]
     
     def generatePWD(self, n, strength):
         if strength not in range(1, len(self.container)+1):
             raise ValueError('Password strength out of range: 1-{}.'.format(len(self.container)))
-        
-        return ''.join(random.choices(string.ascii_uppercase + ''.join(self.container[:strength]), k=n)) 
+
+        return ''.join(random.choices(''.join(self.container[:strength]), k=n)) 
 
 
 #Example
